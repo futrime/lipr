79d4011fb4808193f9293ae214cf86536d341c3b
Zijian Zhang <35801754+futrime@users.noreply.github.com>
2026-02-17 22:45:47 +0800

    feat: back to sequential

--- main.py
+++ main.py
@@ -1,255 +1,202 @@
-import asyncio
-import json
 import logging
 import os
 import shutil
-from asyncio import Semaphore
+import time
+from collections.abc import Iterator
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Final
 
-import aiofiles
-import aiofiles.os
 from git.cmd import Git
 from github import Github
 from github.Auth import Token
 from github.GithubException import GithubException
-from httpx import URL, AsyncClient
-from pydantic import BaseModel, Field
-from semver import VersionInfo
+from httpx import URL, Client
+from pydantic import BaseModel, Field, RootModel
+from semver import Version
 
 BASE_DIR: Final = Path("./workspace/lipr/github.com")
-INDEX_FILE_PATH: Final = Path("./workspace/lipr/index.json")
-MAX_CONCURRENCY: Final = 16
-UUID: Final = "289f771f-2c9a-4d73-9f3f-8492495a924d"
 
 
-class PackageManifestInfo(BaseModel):
+class PackageInfo(BaseModel):
     name: str = ""
     description: str = ""
     tags: list[str] = Field(default_factory=list)
     avatar_url: str = ""
 
 
+class PackageIndexEntry(BaseModel):
+    info: PackageInfo
+    versions: list[Version] = Field(default_factory=list)
+
+
+class PackageIndex(RootModel):
+    root: dict[str, PackageIndexEntry] = Field(default_factory=dict)
+
+
 class PackageManifest(BaseModel):
     format_version: int
     format_uuid: str
     tooth: str
-    version: str
-    info: PackageManifestInfo = Field(default_factory=PackageManifestInfo)
+    version: Version
+    info: PackageInfo = Field(default_factory=PackageInfo)
 
 
-async def fetch_manifest(
-    repository: str, ref: str, *, client: AsyncClient, semaphore: Semaphore
-) -> PackageManifest | None:
-    try:
-        manifest_url = URL(
-            f"https://raw.githubusercontent.com/{repository}/{ref}/tooth.json"
-        )
+def fetch_manifest(
+    repo: str, version: Version | None, *, client: Client
+) -> PackageManifest:
+    if version is None:
+        url = URL(f"https://raw.githubusercontent.com/{repo}/HEAD/tooth.json")
+    else:
+        url = URL(f"https://raw.githubusercontent.com/{repo}/v{version}/tooth.json")
 
-        async with semaphore:
-            response = await client.get(manifest_url)
+    response = client.get(url)
 
-        response.raise_for_status()
+    response.raise_for_status()
 
-        manifest = PackageManifest.model_validate_json(response.content)
+    manifest = PackageManifest.model_validate_json(response.content)
 
-        logging.info(f"Fetched manifest for {repository}@{ref}")
-
-        return manifest
-
-    except Exception as e:
-        logging.error(f"Cannot fetch manifest for {repository}@{ref}: {e}")
-
-        return None
-
-
-async def fetch_versions(repository: str, *, semaphore: Semaphore) -> list[VersionInfo]:
-    try:
-        git = Git()
-
-        repository_url = URL(f"https://github.com/{repository}.git")
-
-        async with semaphore:
-            result: str = await asyncio.to_thread(
-                git.ls_remote, "-t", "--refs", repository_url
-            )
-
-        versions = []
-        for line in result.splitlines():
-            ref = line.split()[1]
-
-            if not ref.startswith("refs/tags/v"):
-                continue
-
-            version_str = ref.removeprefix("refs/tags/v")
-
-            try:
-                versions.append(VersionInfo.parse(version_str))
-
-            except ValueError:
-                continue
-
-        sorted_versions = sorted(versions)
-
-        logging.info(f"Fetched {len(sorted_versions)} versions for {repository}")
-
-        return sorted_versions
-
-    except Exception as e:
-        logging.error(f"Cannot fetch versions for {repository}: {e}")
-
-        return []
-
-
-async def generate_index_file(
-    repositories: list[str],
-    versions: list[list[VersionInfo]],
-    *,
-    client: AsyncClient,
-    semaphore: Semaphore,
-) -> None:
-    async with asyncio.TaskGroup() as tg:
-        tasks = [
-            tg.create_task(
-                fetch_manifest(repo, "HEAD", client=client, semaphore=semaphore)
-            )
-            for repo in repositories
-        ]
-
-    manifests = [t.result() for t in tasks]
-
-    index = {
-        f"github.com/{repo}": {
-            "info": mft.info.model_dump(),
-            "versions": [str(ver) for ver in vers],
-        }
-        for repo, vers, mft in zip(repositories, versions, manifests)
-        if mft is not None
-    }
-
-    content = json.dumps(index, ensure_ascii=False, indent=2, sort_keys=True)
-
-    await aiofiles.os.makedirs(INDEX_FILE_PATH.parent, exist_ok=True)
-    async with aiofiles.open(INDEX_FILE_PATH, "w", encoding="utf-8") as f:
-        await f.write(content)
-
-    logging.info("Generated index file")
-
-
-async def save_manifest(
-    repository: str, version: VersionInfo, *, client: AsyncClient, semaphore: Semaphore
-) -> bool:
-    manifest = await fetch_manifest(
-        repository, f"v{version}", client=client, semaphore=semaphore
+    logging.info(
+        f"Fetched manifest for github.com/{repo}" + (f"@{version}" if version else "")
     )
 
-    if manifest is None:
-        return False
+    return manifest
+
+
+def fetch_versions(repo: str, *, git: Git) -> list[Version]:
+    url = URL(f"https://github.com/{repo}.git")
+
+    result: str = git.ls_remote("-t", "--refs", url)
+
+    versions = []
+    for line in result.splitlines():
+        ref = line.split()[1]
+
+        if not ref.startswith("refs/tags/v"):
+            continue
+
+        ver_str = ref.removeprefix("refs/tags/v")
+
+        try:
+            ver = Version.parse(ver_str)
+        except ValueError:
+            continue
+
+        versions.append(ver)
+
+    versions.sort()
+
+    logging.info(f"Fetched {len(versions)} versions for github.com/{repo}")
+
+    return versions
+
+
+def save_index_file(index: PackageIndex) -> None:
+    path = Path("./workspace/lipr/index.json")
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    content = index.model_dump_json(ensure_ascii=False, indent=2)
+
+    path.write_text(content, encoding="utf-8")
+
+    logging.info(f"Saved index file at {path}")
+
+
+def save_manifest_file(manifest: PackageManifest, repo: str, version: Version) -> None:
+    path = BASE_DIR / repo / str(version) / "tooth.json"
+    path.parent.mkdir(parents=True, exist_ok=True)
 
     content = manifest.model_dump_json(ensure_ascii=False, indent=2)
 
-    path = BASE_DIR / repository / str(version) / "tooth.json"
-    await aiofiles.os.makedirs(path.parent, exist_ok=True)
-    async with aiofiles.open(path, "w", encoding="utf-8") as f:
-        await f.write(content)
+    path.write_text(content, encoding="utf-8")
 
-    logging.info(f"Saved manifest for {repository}@{version}")
-
-    return True
+    logging.info(f"Saved manifest file for github.com/{repo}@{version} at {path}")
 
 
-async def search_repositories(
-    *,
-    semaphore: Semaphore,
-) -> list[str]:
+def search_repositories() -> Iterator[str]:
     if (token := os.getenv("GITHUB_TOKEN")) is None:
         raise RuntimeError("GITHUB_TOKEN environment variable is not set")
 
-    github = Github(auth=Token(token), per_page=100)
-    pagination = github.search_code(UUID, filename="tooth.json", path="/")
+    with Github(auth=Token(token), per_page=100) as github:
+        pagination = github.search_code(
+            "289f771f-2c9a-4d73-9f3f-8492495a924d", filename="tooth.json", path="/"
+        )
 
-    page_idx = 0
-    results = []
-    while True:
-        try:
-            async with semaphore:
-                page = await asyncio.to_thread(pagination.get_page, page_idx)
+        page_idx = 0
+        while True:
+            try:
+                page = pagination.get_page(page_idx)
 
-            if not page:
-                break
+                if not page:
+                    break
 
-            results.extend(item.repository.full_name for item in page)
+                yield from (item.repository.full_name for item in page)
 
-            page_idx += 1
+                page_idx += 1
 
-            await asyncio.sleep(3)
+            except GithubException as ex:
+                if ex.status != 429:
+                    raise
 
-        except GithubException as ex:
-            if ex.status == 429:
-                logging.warning("Exceeded GitHub API rate limit. Waiting for reset...")
-
-                reset_time = github.get_rate_limit().resources.search.reset
                 sleep_time = (
-                    reset_time - datetime.now(timezone.utc)
+                    github.get_rate_limit().resources.search.reset
+                    - datetime.now(timezone.utc)
                 ).total_seconds() + 1
-                await asyncio.sleep(sleep_time)
-            else:
-                raise
 
-    logging.info(f"Found {len(results)} repositories")
+                logging.warning(
+                    f"Exceeded GitHub API rate limit. Waiting {sleep_time} seconds for reset..."
+                )
 
-    return results
+                time.sleep(sleep_time)
+
+    logging.info(f"Found {pagination.totalCount} repositories")
 
 
-async def main() -> None:
+def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
+    # Clean up workspace.
     if BASE_DIR.exists():
         shutil.rmtree(BASE_DIR)
     BASE_DIR.mkdir(parents=True, exist_ok=True)
 
-    semaphore = Semaphore(MAX_CONCURRENCY)
+    git = Git()
 
-    async with AsyncClient() as client:
-        repositories = await search_repositories(
-            semaphore=semaphore,
-        )
+    infos: dict[str, PackageInfo] = {}
+    versions: dict[str, list[Version]] = {}
 
-        async with asyncio.TaskGroup() as tg:
-            tasks = [
-                tg.create_task(fetch_versions(repo, semaphore=semaphore))
-                for repo in repositories
-            ]
-        versions = [t.result() for t in tasks]
+    with Client() as client:
+        for repo in search_repositories():
+            try:
+                manifest = fetch_manifest(repo, version=None, client=client)
+            except Exception as ex:
+                logging.error(f"Failed to fetch manifest for github.com/{repo}: {ex}")
+                continue
 
-        version_tasks = []
-        async with asyncio.TaskGroup() as tg:
-            for repo, vers in zip(repositories, versions):
-                repo_tasks = []
-                for v in vers:
-                    task = tg.create_task(
-                        save_manifest(
-                            repo,
-                            v,
-                            client=client,
-                            semaphore=semaphore,
-                        )
+            infos[repo] = manifest.info
+
+            for ver in fetch_versions(repo, git=git):
+                try:
+                    manifest = fetch_manifest(repo, ver, client=client)
+                except Exception as ex:
+                    logging.error(
+                        f"Failed to fetch manifest for github.com/{repo}@{ver}: {ex}"
                     )
-                    repo_tasks.append((v, task))
-                version_tasks.append(repo_tasks)
+                    continue
 
-        versions = [
-            [v for v, t in repo_tasks if t.result()] for repo_tasks in version_tasks
-        ]
+                save_manifest_file(manifest, repo, ver)
 
-        await generate_index_file(
-            repositories,
-            versions,
-            client=client,
-            semaphore=semaphore,
-        )
+                versions.setdefault(repo, []).append(ver)
+
+    index = PackageIndex(
+        {
+            repo: PackageIndexEntry(info=infos[repo], versions=vers)
+            for repo, vers in versions.items()
+        }
+    )
+
+    save_index_file(index)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
